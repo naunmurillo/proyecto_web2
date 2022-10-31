aa2a196f3e444dfb9f25467883ebee9d613f2cd3
franciscoa10 <58498095+franciscoa10@users.noreply.github.com>
2022-10-30 19:36:00 -0500

    Delete proyecto primer parcial francisco directory

--- proyecto primer parcial francisco/dist/aspirante/dto/aspirante.dto.js
+++
@@ -1,1 +0,0 @@
-//# sourceMappingURL=aspirante.dto.js.map
--- proyecto primer parcial francisco/dist/aspirante/dto/update-aspirante.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { CreateAspiranteDto } from './create-aspirante.dto';
-declare const UpdateAspiranteDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateAspiranteDto>>;
-export declare class UpdateAspiranteDto extends UpdateAspiranteDto_base {
-    nombre: string;
-    correo: string;
-    identificacion: number;
-}
-export {};
