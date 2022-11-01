964f9e21abc5d44ef79a7565dd18c9953a8bf69c
naunmurillo <naunmurillo@outlook.es>
2022-11-01 06:57:11 -0500

    Modificaciones minimas...

--- Trabajo autonomo Parcial I Naun/monilitico/src/app.service.ts
+++ Trabajo autonomo Parcial I Naun/monilitico/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return '<div align = "center">!Hola! Ingresa a la documentacion: <br><br><a href="http://localhost:4000/documentation"><button>Ingresar</button></a></div>';
+    return '<div align = "center">!Hola! Ingresa a la documentacion: <br><br><a href="https://monolitico-parcial1-naun-2022.herokuapp.com/documentation"><button>Ingresar</button></a></div>';
   }
 }
